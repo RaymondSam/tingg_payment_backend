dbc7ef12885f6908104065130ef3c9ca12a81925
RaymondSam <raysamkinyua23@gmail.com>
2023-08-13 13:45:14 +0300

    initial commit

---
+++ tingg/src/main/java/com/technote/tingg/TinggApplication.java
@@ -0,0 +1,13 @@
+package com.technote.tingg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinggApplication.class, args);
+	}
+
+}
---
+++ tingg/src/test/java/com/technote/tingg/TinggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technote.tingg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
